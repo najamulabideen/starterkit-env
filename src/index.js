7cfeeb1af107055290dd075f1005634d6976d848
HLNAJAM <najam@hybridlogics.com>
2018-07-26 19:12:00 +0500

    linting: eslint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './index.css';
 import numeral from 'numeral';
 
+/* eslint-disable no-console */
+
 const courseVal = numeral(1000).format('$0,0.00');
-debugger;
+debugger;//eslint-disable-line
 console.log(`I would like to pay ${courseVal} for this awesome course!`);
