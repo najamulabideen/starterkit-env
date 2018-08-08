0eafe9c2075062a5ca345bce3baee61f099786bb
HLNAJAM <najam@hybridlogics.com>
2018-08-08 20:12:41 +0500

    production deploy Surge

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -16,13 +16,14 @@ app.get('/', function (req, res) {
 
 });
 
-app.get('/users', function (req, res) {
-  res.json([
-    {"id":1, "firstName":"Najam", "lastName":"Tariq", "email":"najamulabideen@gmail.com"},
-    {"id":2, "firstName":"Abdul Qudoos", "lastName":"Mehdi", "email":"abdulqudoos@gmail.com"},
-    {"id":3, "firstName":"Rizwan", "lastName":"Haider", "email":"rizwan.seing@gmail.com"}
-  ]);
-});
+// No need as the app is online on heroku
+// app.get('/users', function (req, res) {
+//   res.json([
+//     {"id":1, "firstName":"Najam", "lastName":"Tariq", "email":"najamulabideen@gmail.com"},
+//     {"id":2, "firstName":"Abdul Qudoos", "lastName":"Mehdi", "email":"abdulqudoos@gmail.com"},
+//     {"id":3, "firstName":"Rizwan", "lastName":"Haider", "email":"rizwan.seing@gmail.com"}
+//   ]);
+// });
 
 app.listen(port, function (err) {
   if (err) {
--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
- return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+ return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://rocky-inlet-40282.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url){
